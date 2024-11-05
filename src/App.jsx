d4b3c5d7f1c69acd29839a8bb56dae7c106a74a6
astridovic <148537371+asturdovic@users.noreply.github.com>
2024-11-06 00:00:13 +0100

    hej

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Onboarding from './Onboarding';
+import Kategori from './Kategori';
+import Profil from './Profil';
+import OpretProfil from './OpretProfil';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Onboarding />} />
+                <Route path='/OpretProfil' element={<OpretProfil/>} />
+                <Route path='/Kategori' element={<Kategori/>} />
+                <Route path='/Profil' element={<Profil/>} />
+                <Route path='/Upload' element={<Upload/>} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/kategori.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./App.css";
+
+const Kategori = ({ onClose }) => {
+    // const onboardingSteps = [
+    //   {
+    //     title: "Velkommen til Lendly",
+    //     image: "/path/to/image1.png",
+    //     text: "Del dine værktøjer og gør hverdagen nemmere for dig selv og andre.",
+    //   },
+    //   {
+    //     title: "Find det du mangler",
+    //     image: "/path/to/image2.png",
+    //     text: "Brug Lendly til at finde værktøj og udstyr i dit lokalområde.",
+    //   },
+    //   {
+    //     title: "Lån og Del",
+    //     image: "/path/to/image3.png",
+    //     text: "Del dine egne ting og tjen lidt ekstra, mens du hjælper andre.",
+    //   },
+    // ];
+  
+    // return (
+    //   <div className="onboarding-container">
+    //     <img
+    //       src={''}
+    //       alt="Close"
+    //       className="close-icon"
+    //       onClick={onClose}
+    //     />
+  
+    //     <Swiper spaceBetween={30} pagination={{ clickable: true }}>
+    //       {onboardingSteps.map((step, index) => (
+    //         <SwiperSlide key={index}>
+    //           <div className="onboarding-slide">
+    //             <h2>{step.title}</h2>
+    //             <img
+    //               src={step.image}
+    //               alt={`Step ${index + 1}`}
+    //               className="onboarding-image"
+    //             />
+    //             <p>{step.text}</p>
+    //           </div>
+    //         </SwiperSlide>
+    //       ))}
+    //     </Swiper>
+  
+    //     <div className="pagination-dots">
+    //       {/* Swiper pagination generates dots automatically */}
+    //     </div>
+    //   </div>
+    // );
+  
+    return <>Hej</>
+  };
+
+  export default Kategori;
